561ed464f4ba87b429fec8c3dcade46940e370ec
BeeLeg <beeleg@cefimmac115.home>
2021-12-01 10:51:50 +0100

    update

--- src/fr/travauxpratique/minibanque/Client.java
+++ src/fr/travauxpratique/minibanque/Client.java
@@ -3,19 +3,19 @@ package fr.travauxpratique.minibanque;
 public class Client {
 
     public int nbComptes = 0;
-    public Compte[]comptes = new Compte[100];
+    private Compte[]comptes = new Compte[100];
     private String nom = new String();
 
-    Client(String nom_du_client){
+    public Client(String nom_du_client){
         this.nom = nom_du_client;
     }
 
-    public String getNom(){
-        return "toto";
+    public String getNom() {
+        return nom;
     }
 
-    public float getSolde() {
-        return 0;
+    public float getSolde(Compte compte) {
+        return compte.getSolde();
     }
 
     public void afficherSolde() {
